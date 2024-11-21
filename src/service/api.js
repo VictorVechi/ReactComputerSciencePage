2d11881d42d2765d29ad14b72b3367a4c8684a96
Victor-Vechi <victor.silva@rentcars.com>
2024-11-21 10:35:28 -0300

    cria api requests

--- src/service/api.js
+++ src/service/api.js
@@ -1,11 +1,23 @@
 import axios from "axios";
 
-class Api {
+export default class Api {
   constructor() {
     this.api = axios.create({
       baseURL: process.env.API_URL,
     });
   }
 
+  // definir as requisições para a API de maneira estatica seguindo um exemplo a seguir
+  // VERIFICAR COMO INSERIR O JWT COMO AUTH NA REQUEST
+  // obs: da uma olhada na documentação do axios para ver como fazer isso
+
+    // static async postUsuario(data) {
+    //     try {
+    //         const response = await this.api.post('/usuario', data);
+    //         return response.data;
+    //     } catch (error) {
+    //         console.error(error)
+    //     }
+    // }
   
-}
+};

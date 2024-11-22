27756e337c4fb400af40b4b9a8675940efe51132
EricoMeger <ericomeger9@gmail.com>
2024-11-21 22:30:06 -0300

    add jwtToken interceptor

--- src/service/Api.js
+++ src/service/Api.js
@@ -5,8 +5,19 @@ export default class Api {
     this.api = axios.create({
       baseURL: process.env.API_URL,
     });
+    
+    this.api.interceptors.request.use((config) => {
+      const token = localStorage.getItem('jwtToken');
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (error) => {
+      return Promise.reject(error);
+    });
   }
-
+  
   // definir as requisições para a API de maneira estatica seguindo um exemplo a seguir
   // VERIFICAR COMO INSERIR O JWT COMO AUTH NA REQUEST
   // obs: da uma olhada na documentação do axios para ver como fazer isso
@@ -19,5 +30,15 @@ export default class Api {
     //         console.error(error)
     //     }
     // }
+    //metodos Usuario
+
+    static async getUsuarioById(id) {
+        try {
+            const response = await this.api.get(`/user/${id}`);
+            return response.data;
+        } catch (error) {
+            console.error(error)
+        }
+    }
   
 };

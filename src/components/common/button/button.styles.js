c7d7c7bee1904eee6e63619528634e0c2ce6f009
EricoMeger <ericomeger9@gmail.com>
2024-11-25 14:06:27 -0300

    remove card & button css

--- src/components/common/button/button.styles.js
+++ src/components/common/button/button.styles.js
@@ -1,38 +1,5 @@
 import styled from 'styled-components';
 
 export const StyledButton = styled.button`
-  width: 10%;
-  height: 25px;
-  border: 2px solid ${(props) => props.theme.orangeP1};
-  border-radius: 5px;
-  background-color: ${(props) => props.theme.orangeP1};
-  color: ${(props) => props.theme.whiteP1};
-  font-size: 16px;
-  font-weight: 800;
-  outline: none;
-  cursor: pointer;
-  transition: 0.3s;
 
-  &:hover {
-    background-color: ${(props) => props.theme.whiteP1};
-    color: ${(props) => props.theme.orangeP1};
-  }
-
-  &:disabled {
-    background-color: ${(props) => props.theme.whiteP1};
-    color: ${(props) => props.theme.orangeP1};
-    border-color: ${(props) => props.theme.orangeP1};
-    cursor: not-allowed;
-    opacity: 0.6;
-  }
-
-  &.primary {
-    background-color: ${(props) => props.theme.blueP1};
-    border-color: ${(props) => props.theme.blueP1};
-  }
-
-  &.secondary {
-    background-color: ${(props) => props.theme.purpleP1};
-    border-color: ${(props) => props.theme.purpleP1};
-  }
 `;
--- src/components/common/card/card.styles.js
+++ src/components/common/card/card.styles.js
@@ -1,39 +1,5 @@
 import styled from 'styled-components';
 
 export const StyledCard = styled.div`
-  border: 1px solid #ddd;
-  border-radius: 8px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-  background-color: #fff;
-  margin: 1rem;
-  padding: 1rem;
-  width: 300px;
-`;
 
-export const CardImage = styled.img`
-  width: 100%;
-  height: auto;
-  border-radius: 8px 8px 0 0;
-`;
-
-export const CardHeader = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  margin-top: 0.5rem;
-`;
-
-export const CardTitle = styled.h2`
-  font-size: 1.5rem;
-  margin: 0.5rem 0 0 0;
-`;
-
-export const CardTag = styled.span`
-  display: inline-block;
-  background-color: #eee;
-  color: #333;
-  padding: 0.25rem 0.5rem;
-  border-radius: 4px;
-  font-size: 0.875rem;
-  margin-bottom: 0.5rem;
 `;

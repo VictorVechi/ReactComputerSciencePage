9aba2d5e63fd5069a53bf8ad6f4c657dffcc9967
IgorSPinto <igorpinto103@gmail.com>
2024-11-25 22:05:49 -0300

    add second section with content and button to Home component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import { StyledHome } from './home.styles';
 import Header from '../../components/common/header/Header';
+import Button from '../../components/common/button/Button';
 
 const Home = () => {
     return (
         <StyledHome>
             <Header/>
             {/* Colocar os outros conteúdos aqui */}
+            <section className='second-section'>
+                <div className='content'>
+                    <div className='text-second-section'>
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur fugiat placeat id ut, omnis explicabo harum nihil assumenda eveniet consectetur asperiores iusto quasi. Quaerat iusto non voluptatibus, illum adipisci earum!
+                        </p>
+                    </div>
+                </div>
+                <div className='right-button'>
+                    <Button texto="Leva pra outro site" href="https://ifpr.edu.br/pinhais/ifpr-pinhais/quem-somos/" />
+                </div>
+            </section>
             
         </StyledHome>
     )

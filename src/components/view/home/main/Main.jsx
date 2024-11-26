4da0d5ee48fef419487d7a028b562b72797c0dea
IgorSPinto <igorpinto103@gmail.com>
2024-11-26 00:08:30 -0300

    add student area section with content and button to main component, and add the styles for the contents

--- src/components/view/home/main/Main.jsx
+++ src/components/view/home/main/Main.jsx
@@ -12,6 +12,24 @@ const Main = () => {
 
     return (
         <StyleMain>
+            <section className='student-area-section'>
+                <div className='content'>
+                    <div className='text-second-section'>
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tenetur fugiat placeat id ut, omnis explicabo harum nihil assumenda eveniet consectetur asperiores iusto quasi. Quaerat iusto non voluptatibus, illum adipisci earum!
+                        </p>
+                    </div>
+                </div>
+                <div className='right-button'>
+                    <a href="https://sei.ifpr.edu.br/sei/modulos/pesquisa/md_pesq_documento_consulta_externa.php?9LibXMqGnN7gSpLFOOgUQFziRouBJ5VnVL5b7-UrE5RWSqtE-LSj0koYd3KUyHCzVmyseU6baq85rW3LNDkcCuYfNj96-xh0PBsewwDdGeZsjV3tOkqRq0z73pqNUXy3&_gl=1*3n7i9g*_ga*NjYxOTI0NDgzLjE2OTk1NzQ2NTc.*_ga_7V1B6HEL02*MTczMjU4NzA4Mi4xNy4xLjE3MzI1ODcwODIuNjAuMC4w" 
+                       target="_blank" 
+                       rel="noopener noreferrer">
+                        <button>
+                            Acesse o Documento
+                        </button>
+                    </a>
+                </div>
+            </section>
             <section className='posts-section'>
                 {posts.map((post, index) => (
                     <div key={index} className='post-card'>
--- src/components/view/home/main/main.styles.js
+++ src/components/view/home/main/main.styles.js
@@ -5,6 +5,9 @@ export const StyleMain = styled.main`
     display: flex;
     justify-content: center;
     align-items: center;
+    flex-direction: column;
+    width: 100%;
+
 
     .posts-section {
         display: flex;
@@ -12,4 +15,52 @@ export const StyleMain = styled.main`
         justify-content: center;
         width: 50%;
     }
+
+    .student-area-section {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 65vh; /* Ocupa a altura total da viewport */
+        background-image: url('/backgroundThreeWizard.png'); /* Caminho relativo à pasta public */
+        background-size: cover;
+        background-position: center;
+        background-repeat: no-repeat;
+        background-attachment: fixed; /* Parallax effect */
+        padding: 0 20px;
+        color: white;
+        width: 100%;
+  }
+
+    .content {
+        width: 80%;
+        max-width: 1200px;
+        display: flex;
+        justify-content: space-between;
+  }
+
+    .text-second-section {
+        flex: 1;
+        font-size: 1.2em;
+        line-height: 1.5;
+        max-width: 600px;
+  }
+
+    .right-button button {
+        color: white;
+        padding: 10px 20px;
+        border: none;
+        border-radius: 5px;
+        cursor: pointer;
+        transition: background-color 0.3s ease;
+        background-color: #ee544f;
+}
+
+
+    @media (max-width: 768px) {
+        .content {
+            flex-direction: column;
+            align-items: center;
+    }
+
+  }
 `;

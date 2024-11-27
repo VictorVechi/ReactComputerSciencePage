8d7adee640ddc9665a0d5be312c31c9c9e2960ac
EricoMeger <ericomeger9@gmail.com>
2024-11-26 21:56:27 -0300

    add posts content preview to card

--- src/components/view/home/posts-section/PostsSection.jsx
+++ src/components/view/home/posts-section/PostsSection.jsx
@@ -24,7 +24,7 @@ const PostsSection = () => {
         <StylePostSection>
             {posts.map((post, index) => (
                 <div key={index} className='post-card'>
-                    <Card title={post.title} tag={post.tags[0].name} />
+                    <Card title={post.title} content={post.content} tag={post.tags[0].name} />
                 </div>
             ))}
         </StylePostSection>

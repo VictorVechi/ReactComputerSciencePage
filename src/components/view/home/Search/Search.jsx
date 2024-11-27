108cce1fb9825b30c220d7f91368f6a0ad4dd348
EricoMeger <ericomeger9@gmail.com>
2024-11-26 23:45:18 -0300

    create field input component

--- src/components/view/home/Search/Search.jsx
+++ src/components/view/home/Search/Search.jsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSearch } from "./search.styles";
+import Field from "../../../common/field/Field";
+import Button from "../../../common/button/Button";
 
+const Search = ({ onSearch }) => {
+    const [searchTerm, setSearchTerm] = useState('');
 
+    const handleInputChange = (e) => {
+        setSearchTerm(e.target.value);
+    };
 
-const Search = () => {
+    const handleSearchClick = () => {
+    //    implementar request pra api
+    };
+
     return (
         <StyleSearch>
             <h1>Search</h1>
+            <Field
+                type="text"
+                value={searchTerm}
+                onChange={handleInputChange}
+                placeholder="Pesquisa por titulo"
+            />
+            <Button texto="Pesquisar" onClick={handleSearchClick} />
         </StyleSearch>
     );
-}
+};
+
 export default Search;

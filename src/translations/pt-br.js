2eb00ae0fdf89ad22e7151e1298d431d1ba295fe
VictorVechi <victorhvechi@gmail.com>
2024-11-27 10:54:52 -0300

    create translation

---
+++ src/translations/pt-br.js
@@ -0,0 +1,11 @@
+
+export const translation = {
+    studentSection: {
+        main: {
+            description: "Este site foi desenvolvido com o objetivo de auxiliar alunos e professores do curso de Ciências da Computação do IFPR, campus Pinhais. Para obter mais informações, como horários de aulas e calendário acadêmico, acesse a área destinada aos alunos."
+        },
+        button: {
+            text: "Área do Aluno"
+        }
+    }
+}

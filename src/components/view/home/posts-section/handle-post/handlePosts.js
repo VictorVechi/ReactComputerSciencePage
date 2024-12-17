a05575bcfefb9ba7364d30e2d187659acb2e7c9b
EricoMeger <ericomeger9@gmail.com>
2024-12-16 21:13:48 -0300

    add id on fake posts

--- src/components/view/home/posts-section/handle-post/handlePosts.js
+++ src/components/view/home/posts-section/handle-post/handlePosts.js
@@ -2,31 +2,37 @@ import Api from "../../../../../service/gateway/Api";
 
 const fakePosts = [
     {
+        id: 1,
         title: 'Post não encontrado',
         content: 'Conteudo do post',
         tags: [{ name: '404' }]
     },
     {
+        id: 2,
         title: 'Post não encontrado',
         content: 'Conteudo do post',
         tags: [{ name: '404' }]
     },
     {
+        id: 3,
         title: 'Post não encontrado',
         content: 'Conteudo do post',
         tags: [{ name: '404' }]
     },
     {
+        id: 4,
         title: 'Post não encontrado',
         content: 'Conteudo do post',
         tags: [{ name: '404' }]
     },
     {
+        id: 5,
         title: 'Post não encontrado',
         content: 'Conteudo do post',
         tags: [{ name: '404' }]
     },
     {
+        id: 6,
         title: 'Post não encontrado',
         content: 'Conteudo do post',
         tags: [{ name: '404' }]

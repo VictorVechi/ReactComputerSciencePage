8c3e9ee71e0f82615db532856f92760c9141fac2
IgorSPinto <igorpinto103@gmail.com>
2024-12-17 23:18:03 -0300

    update footer component with copyright text and social media links

--- src/components/common/footer/footer.styles.js
+++ src/components/common/footer/footer.styles.js
@@ -7,6 +7,34 @@ export const FooterStyled = styled.footer`
     display: flex;
     justify-content: space-between;
     align-items: center;
-    background-color: ${theme.blueP3};
+    background-color: ${theme.purpleP1};
     color: ${theme.white};
 `
+
+export const FooterText = styled.p`
+    margin-left: 20px;
+    font-size: 1.0rem;
+`
+export const SocialLinks = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+
+    a {
+        color: ${theme.white};
+        margin-left: 15px;
+        text-decoration: none;
+        font-size: 1.2rem;
+        display: flex;
+        align-items: center;
+
+        svg {
+            margin-right: 26px;  
+            transition: color 0.3s;
+        }
+
+        &:hover svg {
+            color: ${theme.purpleP3}; 
+        }
+    }
+`;

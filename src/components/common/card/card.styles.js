f40b891073d07dc4160bcda5b25d1894ac0e32c6
EricoMeger <ericomeger9@gmail.com>
2024-12-17 21:23:48 -0300

    card hover

--- src/components/common/card/card.styles.js
+++ src/components/common/card/card.styles.js
@@ -12,6 +12,12 @@ export const StyledCard = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
+  transition: transform 0.3s, box-shadow 0.3s;
+
+  &:hover {
+    transform: translateY(-5px);
+    box-shadow: 0 4px 8px ${theme.orangeP3};
+  }
 
   .card-header {
     display: flex;

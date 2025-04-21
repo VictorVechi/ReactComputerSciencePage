ddb160bfbf4405372731a219302a8d81931abe2f
EricoMeger <ericomeger9@gmail.com>
2025-04-21 18:59:15 -0300

    fetch and update user data

--- src/components/view/dashboard/user/edit-user/edit-user-info/UserUpdateInfoForm.jsx
+++ src/components/view/dashboard/user/edit-user/edit-user-info/UserUpdateInfoForm.jsx
@@ -2,30 +2,41 @@ import React, { useState, useEffect } from "react";
 import { StyledUserUpdateInfoForm } from "./userUpdateInfoForm.styles";
 import { useNavigate } from "react-router-dom";
 import Api from "../../../../../../service/gateway/Api";
+import { LocalStorageEnum } from "../../../../../../enum/LocalStorageEnum";
 
 const UserUpdateInfoForm = () => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
+    const apiInstance = Api.getInstance();
+    const userID = localStorage.getItem(LocalStorageEnum.USER_ID);
     const navigate = useNavigate();
 
-    // useEffect(() => {
-    //     fetchRoles(setRoles);
-    // }, []);
+    useEffect(() => {
+        const getUserData = async () => {
+            const user_data = await apiInstance.getUsuarioById(userID);
+
+            setName(user_data.user.name);
+            setEmail(user_data.user.email);
+        }
+
+        getUserData();
+    }, []);
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        console.log("working");
-        // try {
-        //     const apiInstance = Api.getInstance();
-        //     const data = {
-        //         name,
-        //         email,
-        //     };
-        //     await apiInstance.postUsuarioRegister(data);
-        //     navigate("/dashboard");
-        // } catch (error) {
-        //     console.error("Erro ao criar usuário:", error);
-        // }
+
+        try {
+            const data = {
+                id: userID,
+                name,
+                email,
+            };
+
+            await apiInstance.putUsuarioData(data);
+            navigate("/dashboard");
+        } catch (error) {
+            console.error("Erro ao criar usuário:", error);
+        }
     };
 
     return (

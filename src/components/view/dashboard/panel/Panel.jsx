f7d4277124e7efa24d703ffe2047d6164e72fd19
EricoMeger <ericomeger9@gmail.com>
2025-04-23 20:19:54 -0300

    define user interactions access control

--- src/components/view/dashboard/panel/Panel.jsx
+++ src/components/view/dashboard/panel/Panel.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { StyledOptionsPanel } from "./panel.styles";
 import { useNavigate } from "react-router-dom";
 import Button from "../../../common/button/Button";
-import UserEditOptions from "../user/edit-user/UserEditOptions";
+import UserOptions from "../user/UserOptions";
 
 const OptionsPanel = ({ activeOption }) => {
     const navigate = useNavigate();
@@ -29,31 +29,34 @@ const OptionsPanel = ({ activeOption }) => {
     return (
         <StyledOptionsPanel>
             <h2>{optionName}</h2>
-            <div className="button-container">
-                <Button
-                    texto={`Cadastrar Novo ${optionName}`}
-                    onClick={() => handleNavigation(`/dashboard/${activeOption}/create-${activeOption}`)}
-                />
-            </div>
 
             {activeOption === "user" ? (
-                <UserEditOptions />
+                <UserOptions />
             ) : (
-                <div className="button-container">
-                    <Button
-                        texto={`Editar ${optionName}`}
-                        onClick={() => handleNavigation(`/dashboard/${activeOption}/edit-${activeOption}`)}
-                    />
-                </div>
+                <>
+                    <div className="button-container">
+                        <Button
+                            texto={`Cadastrar Novo ${optionName}`}
+                            onClick={() => handleNavigation(`/dashboard/${activeOption}/create-${activeOption}`)}
+                        />
+                    </div>
+
+                    <div className="button-container">
+                        <Button
+                            texto={`Editar ${optionName}`}
+                            onClick={() => handleNavigation(`/dashboard/${activeOption}/edit-${activeOption}`)}
+                        />
+                    </div>
+
+
+                    <div className="button-container">
+                        <Button
+                            texto={`Deletar ${optionName}`}
+                            onClick={() => handleNavigation(`/dashboard/${activeOption}/delete-${activeOption}`)}
+                        />
+                    </div>
+                </>
             )}
-
-            <div className="button-container">
-                <Button
-                    texto={`Deletar ${optionName}`}
-                    onClick={() => handleNavigation(`/dashboard/${activeOption}/delete-${activeOption}`)}
-                />
-            </div>
-
         </StyledOptionsPanel>
     );
 };

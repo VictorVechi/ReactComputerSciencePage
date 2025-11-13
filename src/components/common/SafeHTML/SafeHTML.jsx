aa28a7fa2468ffd1fcbbcdcc5d53ddc865225365
Eroshla <eroshla@hotmail.com>
2025-11-13 20:13:53 -0300

    clean/Remove safeHMTL

--- src/components/common/SafeHTML/SafeHTML.jsx
+++
@@ -1,8 +0,0 @@
-import { useSanitizedHTML } from '../../../utils/sanitize';
-
-export const SafeHTML = ({ content, className }) => {
-    const sanitizedContent = useSanitizedHTML(content);
-    return <div className={className} dangerouslySetInnerHTML={sanitizedContent} />;
-};
-
-export default SafeHTML;
